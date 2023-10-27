a788ed6b5103e99330eea611d16f6f1ba1bbedcf
Rainer Poisel <rainer@honeytreelabs.com>
2023-10-27 10:29:35 +0200

    chore: make GUI responsive

--- cmd/gui/gui.go
+++ cmd/gui/gui.go
@@ -14,14 +14,8 @@ func main() {
 	adapter := bluetooth.DefaultAdapter
 	adapter.Enable()
 	app := tview.NewApplication()
-	grid := tview.NewGrid().
-		SetRows(3, 0, 3).
-		SetColumns(50, 0, 50).
-		SetBorders(true).
-		AddItem(newPrimitive("Header"), 0, 0, 1, 3, 0, 0, false).
-		AddItem(newPrimitive("Footer"), 2, 0, 1, 3, 0, 0, false)
-
-	menu := newPrimitive("Menu")
+	grid := tview.NewGrid().SetBorders(true)
+	macsView := newPrimitive("Menu")
 	sideBar := newPrimitive("Side Bar")
 
 	var macs map[string]struct{}
@@ -29,7 +23,7 @@ func main() {
 	list.
 		AddItem("Start scan", "Selecting this starts a scan", 's', func() {
 			macs = make(map[string]struct{})
-			textView := menu.(*tview.TextView)
+			textView := macsView.(*tview.TextView)
 			textView.SetText("")
 			go adapter.Scan(func(adapter *bluetooth.Adapter, device bluetooth.ScanResult) {
 				macs[device.Address.String()] = struct{}{}
@@ -38,7 +32,7 @@ func main() {
 		AddItem("Stop scan", "Selecting this stops the scan", 't', func() {
 			adapter.StopScan()
 			text := ""
-			textView := menu.(*tview.TextView)
+			textView := macsView.(*tview.TextView)
 			for mac := range macs {
 				text = mac + "\n" + text
 			}
@@ -49,14 +43,14 @@ func main() {
 		})
 
 	// Layout for screens narrower than 100 cells (menu and side bar are hidden).
-	grid.AddItem(list, 0, 0, 0, 0, 0, 0, false).
-		AddItem(menu, 1, 0, 1, 3, 0, 0, false).
-		AddItem(sideBar, 0, 0, 0, 0, 0, 0, false)
+	grid.AddItem(list /*     */, 0, 0, 1, 1, 0, 0, false).
+		AddItem(macsView /*  */, 1, 0, 1, 1, 0, 0, false).
+		AddItem(sideBar /*   */, 2, 0, 1, 1, 0, 0, false)
 
 	// Layout for screens wider than 100 cells.
-	grid.AddItem(list, 1, 0, 1, 1, 0, 100, false).
-		AddItem(menu, 1, 1, 1, 1, 0, 100, false).
-		AddItem(sideBar, 1, 2, 1, 1, 0, 100, false)
+	grid.AddItem(list /*     */, 0, 0, 1, 1, 0, 150, false).
+		AddItem(macsView /*  */, 0, 1, 1, 1, 0, 150, false).
+		AddItem(sideBar /*   */, 0, 2, 1, 1, 0, 150, false)
 
 	if err := app.SetRoot(grid, true).SetFocus(list).Run(); err != nil {
 		panic(err)
